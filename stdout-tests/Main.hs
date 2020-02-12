31d7aa3144e16308020ea037c0929acd6380bece
Leonhard Markert <curiousleo@users.noreply.github.com>
2020-02-12 16:45:55 +0100

    Fully implement splittable test

--- stdout-tests/Main.hs
+++ stdout-tests/Main.hs
@@ -13,6 +13,7 @@ import System.Environment (getArgs)
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Builder as BS
 import qualified Data.ByteString.Builder.Prim as BSP
+import qualified Data.ByteString.Builder.Extra as BSE
 import qualified System.IO as IO
 import qualified System.Random as Rand
 import qualified System.Random.MWC as MWC
@@ -21,8 +22,8 @@ import qualified System.Random.SplitMix as SplitMix
 random64 :: Rand.RandomGen g => g -> (Word64, g)
 random64 = Rand.random
 
-bla :: Rand.StdGen -> (Rand.StdGen, BS.Builder)
-bla gPrev =
+stdGenSplit :: Rand.StdGen -> (BS.Builder, Rand.StdGen)
+stdGenSplit gPrev =
   let (gNext, g) = Rand.split gPrev
       (gL, gR) = Rand.split g
       (gLL, gLR) = Rand.split gL
@@ -31,9 +32,21 @@ bla gPrev =
       rLR = fst $ random64 gLR
       rRL = fst $ random64 gRL
       rRR = fst $ random64 gRR
-   in (gNext, BSP.primMapListFixed BSP.emptyF [rLL, rLR, rRL, rRR])
+   in (BSP.primFixed (BSP.word64Host BSP.>*< BSP.word64Host BSP.>*< BSP.word64Host BSP.>*< BSP.word64Host) (rLL, (rLR, (rRL, rRR))), gNext)
 
+smGenSplit :: SplitMix.SMGen -> (BS.Builder, SplitMix.SMGen)
+smGenSplit gPrev =
+  let (gNext, g) = Rand.split gPrev
+      (gL, gR) = Rand.split g
+      (gLL, gLR) = Rand.split gL
+      (gRL, gRR) = Rand.split gR
+      rLL = fst $ SplitMix.nextWord64 gLL
+      rLR = fst $ SplitMix.nextWord64 gLR
+      rRL = fst $ SplitMix.nextWord64 gRL
+      rRR = fst $ SplitMix.nextWord64 gRR
+   in (BSP.primFixed (BSP.word64Host BSP.>*< BSP.word64Host BSP.>*< BSP.word64Host BSP.>*< BSP.word64Host) (rLL, (rLR, (rRL, rRR))), gNext)
 main :: IO ()
+
 main = do
   let !stdout = IO.stdout
       -- !sm64Gen = SplitMix.newSMGen
@@ -54,17 +67,35 @@ main = do
             -- let bs = fromByteArray Seq $ toByteArray arr :: Array P Ix1 Word64
             -- let bs' = toBuilder BS.word64LE bs
             -- BS.hPutBuilder stdout bs'
-            BS.hPutBuilder stdout (BS.word64BE v)
+            BS.hPutBuilder stdout (BSE.word64Host v)
+    ["random-split"] -> do
+        ref <- newIORef $ Rand.mkStdGen 1337
+        forever $ do
+            !gen <- readIORef ref
+            let !(v, gen') = stdGenSplit gen
+            -- let (gen', arr) = randomArraySPureSeq gen random64 sz :: (Rand.StdGen, Array P Ix1 Word64)
+            writeIORef ref gen'
+            -- let bs = fromByteArray Seq $ toByteArray arr :: Array P Ix1 Word64
+            -- let bs' = toBuilder BS.word64LE bs
+            -- BS.hPutBuilder stdout bs'
+            BS.hPutBuilder stdout v
     ["splitmix"] -> do
         !ref <- newIORef $ SplitMix.mkSMGen 1337
         forever $ do
           !gen <- readIORef ref
           let !(v, gen') = SplitMix.nextWord64 gen
           writeIORef ref gen'
-          BS.hPutBuilder stdout (BS.word64BE v)
+          BS.hPutBuilder stdout (BSE.word64Host v)
+    ["splitmix-split"] -> do
+        !ref <- newIORef $ SplitMix.mkSMGen 1337
+        forever $ do
+          !gen <- readIORef ref
+          let !(v, gen') = smGenSplit gen
+          writeIORef ref gen'
+          BS.hPutBuilder stdout v
     ["mwc"] -> do
         !gen <- MWC.create
         forever $ do
           !v <- MWC.uniform gen
-          BS.hPutBuilder stdout (BS.word64BE v)
+          BS.hPutBuilder stdout (BSE.word64Host v)
 
