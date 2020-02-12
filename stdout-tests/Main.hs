38b0773122b84556f52ee8d7f58b09f9e9de227f
Leonhard Markert <curiousleo@users.noreply.github.com>
2020-02-12 14:42:38 +0100

    wip

--- stdout-tests/Main.hs
+++ stdout-tests/Main.hs
@@ -1,9 +1,14 @@
+{-# LANGUAGE BangPatterns #-}
+
 module Main where
+
+-- import Lib
 
 import Control.Monad (forever)
 import Data.IORef (newIORef, readIORef, writeIORef)
-import Data.Word (Word64)
+import Data.Word (Word64, Word32, Word8, Word16)
 import System.Environment (getArgs)
+import Data.Massiv.Array
 
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Builder as BS
@@ -12,26 +17,41 @@ import qualified System.Random as Rand
 import qualified System.Random.MWC as MWC
 import qualified System.Random.SplitMix as SplitMix
 
+random64 :: Rand.RandomGen g => g -> (Word64, g)
+random64 = Rand.random
+
 main :: IO ()
 main = do
-  let stdout = IO.stdout
+  let !stdout = IO.stdout
+      -- !sm64Gen = SplitMix.newSMGen
+      -- !sz = Sz1 1048576
+
   IO.hSetBinaryMode stdout True
   IO.hSetBuffering stdout (IO.BlockBuffering Nothing)
+
   args <- getArgs
   case args of
-    ["random"] -> forever $ do
-        v <- Rand.randomIO
-        BS.hPut stdout (BS.singleton v)
-    ["splitmix"] -> do
-        ref <- newIORef $ SplitMix.mkSMGen 1337
+    ["random"] -> do
+        ref <- newIORef $ Rand.mkStdGen 1337
         forever $ do
-          gen <- readIORef ref
-          let (v, gen') = SplitMix.nextWord64 gen
+            !gen <- readIORef ref
+            let !(v, gen') = random64 gen
+            -- let (gen', arr) = randomArraySPureSeq gen random64 sz :: (Rand.StdGen, Array P Ix1 Word64)
+            writeIORef ref gen'
+            -- let bs = fromByteArray Seq $ toByteArray arr :: Array P Ix1 Word64
+            -- let bs' = toBuilder BS.word64LE bs
+            -- BS.hPutBuilder stdout bs'
+            BS.hPutBuilder stdout (BS.word64BE v)
+    ["splitmix"] -> do
+        !ref <- newIORef $ SplitMix.mkSMGen 1337
+        forever $ do
+          !gen <- readIORef ref
+          let !(v, gen') = SplitMix.nextWord64 gen
           writeIORef ref gen'
           BS.hPutBuilder stdout (BS.word64BE v)
     ["mwc"] -> do
-        gen <- MWC.create
+        !gen <- MWC.create
         forever $ do
-          v <- MWC.uniform gen
-          BS.hPut stdout (BS.singleton v)
+          !v <- MWC.uniform gen
+          BS.hPutBuilder stdout (BS.word64BE v)
 
